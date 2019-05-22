c3a276955be402c3e42962bc4bfb94c04b3c11e2
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2019-05-21 22:42:30 -0700

    aligned naming convention

--- Projects/BRM.EventBroker/Properties/AssemblyInfo.cs
+++ Projects/BRM.EventBroker/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EventBroker")]
+[assembly: AssemblyTitle("BRM.EventBroker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EventBroker")]
+[assembly: AssemblyProduct("BRM.EventBroker")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
